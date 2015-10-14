3f0fbc0a73bcfd98815a715395c5f8e2be563a59
minex2222 <kazumasa.higashimine@gmail.com>
2015-10-14 15:09:15 +0000

    check-01

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,6 @@ Rails.application.routes.draw do
   post   'login'   => 'sessions#create'
   delete 'logout'  => 'sessions#destroy'
 
-  resources :users
+  resources :microposts
   resources :sessions, only: [:new, :create, :destroy]
 end
